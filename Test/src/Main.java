bbcc00bc7ab82ffb21fb967ea0bc5e7e57725dcc
VadimB <eagle-vadim@mail.ru>
2022-03-18 17:41:29 +0700

    main changed

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -7,22 +7,5 @@ public class Main {
 
         String search = "delette";
 
-        StringBuilder str = new StringBuilder("My name");
-
-        int start = str.indexOf(search);
-        int end = str.indexOf(" ",start);
-
-        str.replace(start,end,"HI!");
-       // String sub = str.substring(start,end);
-        System.out.println(str);
-
-        str.deleteCharAt(5);
-        System.out.println(str);
-
-        str.delete(2,5);
-        System.out.println(str);
-
-        str.insert(8,9);
-        System.out.println(str);
     }
 }
